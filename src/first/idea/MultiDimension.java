7d36c27b55133afd17a41202ebfbc2457d4ea0e3
GunjanPahwa <gunjan19pahwa@gmail.com>
2025-11-27 21:00:23 +0530

    Initial commit

---
+++ src/first/idea/MultiDimension.java
@@ -0,0 +1,34 @@
+package first.idea;
+import java.util.Arrays;
+import java.util.Scanner;
+public class MultiDimension {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        /*
+        1 2 3
+        4 5 6
+        7 8 9
+         */
+//        int[][] arr=new int[3][3];
+////
+//        int[][] arr={
+//                {1,2,3},
+//                {4,5},
+//                {6,7,8,9}
+//        };
+//        System.out.println(Arrays.toString(arr));
+        int[][] arr=new int[3][2];
+        System.out.println(arr.length);
+        for(int row=0;row<arr.length;row++){
+            for(int col=0;col<arr[row].length;col++){
+                arr[row][col]=in.nextInt();
+            }
+        }
+        for(int row=0;row<arr.length;row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
