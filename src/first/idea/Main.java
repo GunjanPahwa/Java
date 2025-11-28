06c5c86d53261b2b8adc98440e59bcd42c5351d4
GunjanPahwa <gunjan19pahwa@gmail.com>
2025-11-28 16:43:41 +0530

    Initial commit

---
+++ src/first/idea/Main.java
@@ -0,0 +1,27 @@
+package first.idea;
+import java.util.Arrays;
+public class Main{
+    public static void main(String[] args) {
+        int[] nums={23,45,1,2,8,19,-3,-11,28};
+        int target=19;
+        int ans=linearsearch(nums,target);
+        System.out.println(ans);
+
+    }
+    static int linearsearch(int[] arr, int target){
+        if(arr.length==0){
+            return-1;
+        }
+        for(int i=0;i<arr.length;i++){
+            int element=arr[i];
+            if(element==target){
+                return i;
+            }
+
+
+        }
+        return -1;
+
+    }
+
+}
