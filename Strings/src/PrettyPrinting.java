d022b913a1edd716688735af2e36b5dcaf1fc027
GunjanPahwa <gunjan19pahwa@gmail.com>
2026-01-01 20:45:23 +0530

    Pretty Printing

---
+++ Strings/src/PrettyPrinting.java
@@ -0,0 +1,12 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a=453.1234f;
+        System.out.printf("formatted number is %.2f",a);
+        System.out.println();
+        System.out.println(Math.PI);
+        System.out.printf("Pie: %.2f",Math.PI);
+        System.out.println();
+        System.out.printf("Hello my name is %s and I am %s","Gunjan","cool");
+
+    }
+}
